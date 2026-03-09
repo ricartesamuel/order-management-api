bd64038f980c30e9de2c48c5da13f239686b9c95
Samuel <ricarte.samuelz@gmail.com>
2026-03-09 15:16:49 -0300

    feat: integrate order routes into express server

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 
 // routes in correct order
-router.post("/order", orderController.createOrder);
-router.get("/order/list", orderController.listOrders);
-router.get("/order/:orderId", orderController.getOrderById);
-router.put("/order/:orderId", orderController.updateOrder);
-router.delete("/order/:orderId", orderController.deleteOrder);
+router.post("/", orderController.createOrder);
+router.get("/list", orderController.listOrders);
+router.get("/:orderId", orderController.getOrderById);
+router.put("/:orderId", orderController.updateOrder);
+router.delete("/:orderId", orderController.deleteOrder);
 
 module.exports = router;
